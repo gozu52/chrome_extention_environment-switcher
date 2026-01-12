becaa74099083202729f15576dc34bc690d8ad90
tomoki.gozu <tomoki.gozu@gmail.com>
2026-01-12 16:01:23 +0900

    Initial commit: Basic Chrome extension structure

---
+++ poopup.js
@@ -0,0 +1,7 @@
+// テストボタンのクリックイベント
+document.getElementById('testBtn').addEventListener('click', () => {
+  alert('Environment Switcher が動作しています！');
+});
+
+// 拡張機能が読み込まれたときの処理
+console.log('Environment Switcher popup loaded');
